e11ced3a58ab9f8cf16f336f7943c16b0dc49f12
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-10-26 23:27:07 +0200

    new lock

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,20 +1,20 @@
 <?php return array(
     'root' => array(
-        'name' => '__root__',
+        'name' => 'vendor/package',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'd7a9527e2afbba7573dc1c64a05a2a017b44a4c5',
-        'type' => 'library',
+        'reference' => '9ef236cf2307e7c4eaba6f1eebde6207af9dc259',
+        'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        '__root__' => array(
+        'vendor/package' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'd7a9527e2afbba7573dc1c64a05a2a017b44a4c5',
-            'type' => 'library',
+            'reference' => '9ef236cf2307e7c4eaba6f1eebde6207af9dc259',
+            'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
